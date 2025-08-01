4acef1c3d694e48d98bf91b208b7911b33e2945e
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2025-08-01 15:08:03 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+    paddingBottom: 20
+  },
+});
